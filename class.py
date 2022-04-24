80d6cb6fd3e7a81db21e145217bcfaee7da205a7
ARMsavage <83109561+ARMsavage@users.noreply.github.com>
2022-04-24 18:39:25 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,100 @@
+from traceback import print_tb
+
+
+def __init__(self,model,color,company,speed_limit):
+    self.color=color
+    self.company=company
+    self.model=model
+    self.speed_limit=speed_limit
+                  
+def start(self):
+    print("started")              
+    
+def stop(self):
+    print("stopped")
+
+def accelarate(self) :
+     print("accelarated")
+
+def changeGear(self)                          :
+    print("changeGear")              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
